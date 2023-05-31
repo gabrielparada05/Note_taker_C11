79c2c90a500a18006013698aae0fee608d64e482
Gabriel Parada <gabrielparada05@gmail.com>
2023-05-31 16:59:56 -0400

    api config ok

---
+++ Develop/routes/notes.js
@@ -0,0 +1,43 @@
+const fb = require('express').Router();
+const fs = require('fs');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the note
+fb.get('/', (req, res) => {
+  console.info(`${req.method} request received for note`);
+
+ readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for submitting note
+fb.post('/', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to submit note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
+
+module.exports = fb;
---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const api = require('./routes/index.js');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for note page
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
